339b6daf34cef4d26de8cb3104b0f05c1783d34d
Marshall Rose <mrose17@gmail.com>
2016-08-14 15:56:39 -0700

    0.8.39

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ var addresses = [
   '3PnrcAMkR7BfwkR1q3epPe2K52eYAZnu5s'
 ]
 addresses.forEach(function (address) {
-  require('.').getBalance(address, { allP: true, timeout: 500 }, function (err, provider, result) {
-    if (err) return console.log('address=' + address + ' provider=' + provider.name + ' ' + err.toString())
+  require('.').getBalance(address, { allP: true, timeout: 1000 }, function (err, provider, result) {
+    if (err) return console.log('address=' + address + ' provider=' + (provider || {}).name + ' ' + err.toString())
 
     console.log('address=' + address + ' provider=' + provider.name + ' balance=' + result)
   })
